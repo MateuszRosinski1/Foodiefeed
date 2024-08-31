4c92a5d9ff80cd3601a373f2e76c22d45b3b76e0
Mateusz Rosinski <mateusz.rosinski111666@gmail.com>
2024-08-31 04:24:20 +0200

    further post contentview implementation

--- Foodiefeed-api/obj/Debug/net8.0-windows10.0.19041/Foodiefeed-api.AssemblyInfo.cs
+++ Foodiefeed-api/obj/Debug/net8.0-windows10.0.19041/Foodiefeed-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodiefeed-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af314ed4a5e03b1cb03d2aee7e70e6e327bd692f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodiefeed-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodiefeed-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Foodiefeed/views/windows/contentview/PostView.xaml.cs
+++ Foodiefeed/views/windows/contentview/PostView.xaml.cs
@@ -5,11 +5,59 @@ namespace Foodiefeed.views.windows.contentview;
 public partial class PostView : ContentView
 {
 	[MaxLength(10)]
-    public byte[] Images { get; set; }
+    public byte[]? Images { get; set; }
+
+	public List<CommentView> Comments { get; set; }
 
     public PostView()
 	{
-		InitializeComponent();	
+		Comments = new List<CommentView>();
+		Comments.Add(new CommentView());
+        Comments.Add(new CommentView());
+        Comments.Add(new CommentView());
+        Comments.Add(new CommentView());
+        Comments.Add(new CommentView());
+        Comments.Add(new CommentView());
+        Comments.Add(new CommentView());
+        Comments.Add(new CommentView());
+        Comments.Add(new CommentView());
+        Comments.Add(new CommentView());
+
+        InitializeComponent();	
+
+		if(Comments is null) { return; }
+
+		foreach(CommentView comment in Comments)
+		{
+			CommentSection.Children.Add(comment);
+		}
 	}
-	
+
+    private async void ExpandCommentSection(object sender, EventArgs e)
+    {
+       // CommentSectionScroll.MaximumHeightRequest = 600;
+
+        await MainThread.InvokeOnMainThreadAsync(() => {
+            var animation = new Animation(v => CommentSectionScroll.MaximumHeightRequest = v, 300, 600);
+            animation.Commit(this, "TrackListSideBarAnimation", 16, 500, Easing.CubicIn);
+        });
+
+        Button b = (Button)sender;
+        b.Clicked -= ExpandCommentSection;
+        b.Clicked += CondenseCommentSection;
+    }
+
+    private async void CondenseCommentSection(object sender, EventArgs e)
+    {
+        await MainThread.InvokeOnMainThreadAsync(() => {
+            var animation = new Animation(v => CommentSectionScroll.MaximumHeightRequest = v, 600, 300);
+            animation.Commit(this, "TrackListSideBarAnimation", 16, 500, Easing.CubicIn);
+        });
+
+        Button b = (Button)sender;
+
+        b.Clicked -= CondenseCommentSection;
+
+        b.Clicked += ExpandCommentSection;
+    }
 }
