9ba054739d1d769eb84b2eeecb164560be03f7cc
Mateusz Rosinski <mateusz.rosinski111666@gmail.com>
2024-09-07 23:29:21 +0200

    new settings design

--- Foodiefeed/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Foodiefeed.AssemblyInfo.cs
+++ Foodiefeed/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Foodiefeed.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodiefeed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+792e392605161384b40b1f1df52a00f4f946d5a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodiefeed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodiefeed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
