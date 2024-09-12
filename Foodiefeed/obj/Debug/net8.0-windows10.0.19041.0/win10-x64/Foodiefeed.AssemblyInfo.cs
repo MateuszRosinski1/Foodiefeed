e706e769c5340ffa9064b82f3cfcb47a9d12a5f7
Mateusz Rosinski <mateusz.rosinski111666@gmail.com>
2024-09-12 12:49:36 +0200

    BindableProperties for postView and expandor/collapsor for post text content

--- Foodiefeed/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Foodiefeed.AssemblyInfo.cs
+++ Foodiefeed/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Foodiefeed.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodiefeed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2c56bee3350ba8e3e4910524a9d8af359038a0b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3532440299e607f420055be757a4bf37ca5b55e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodiefeed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodiefeed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
